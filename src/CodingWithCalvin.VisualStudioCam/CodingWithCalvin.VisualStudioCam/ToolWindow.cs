afaa781ad9a12fa4809f199e05cb2abaac913c1d
Calvin A. Allen <calvin.allen@outlook.com>
2023-06-05 09:26:16 -0400

    Initial commit

---
+++ src/CodingWithCalvin.VisualStudioCam/CodingWithCalvin.VisualStudioCam/ToolWindow.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWithCalvin.VisualStudioCam
+{
+    [Guid("79FC33BF-8DA0-477A-B315-5A48A38FD54D")]
+    public class ToolWindow : ToolWindowPane
+    {
+        public ToolWindow()
+        {
+            Caption = "Webcam";
+            Content = new WebcamView();
+        }
+    }
+}
